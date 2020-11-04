cb4955b6a783243412933bdd5702fb8fa3a85be5
lexysline <lexysline@gmail.com>
2020-11-04 15:52:16 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Cocktail from "./components/Cocktail";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Cocktail />
+    </div>
+  );
+}
---
+++ src/components/Cocktail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Cocktail = () => {
+  return (
+    <>
+      <p>Cocktail component</p>
+    </>
+  );
+};
+
+export default Cocktail;
